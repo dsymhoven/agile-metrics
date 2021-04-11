4f639dccbf0bbac72a067a35bc4534d77bdd0e4f
dsymhoven <symhoven@googlemail.com>
2021-04-11 15:21:00 +0200

    still super dirty adds mc simulation and checks adds percentile

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import config
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
+import random
 import datetime as dt
 
 
 
-PBI_finished = [6, 0, 6, 11, 5, 5, 6, 7, 11, 10, 10, 3, 5, 5, 13, 10, 14, 12, 10, 14, 4, 14, 11, 8, 13, 8]
+PBI_finished_per_sprint = [6, 0, 6, 11, 5, 5, 6, 7, 11, 10, 10, 3, 5, 5, 13, 10, 14, 12, 10, 14, 4, 14, 11, 8, 13, 8]
 number_PBI_to_be_finished = 260
-PBI_cumulative = np.cumsum(PBI_finished)
-PBI_average = np.average(PBI_finished)
-PBI_stddeviation = np.std(PBI_finished)
+PBI_cumulative = np.cumsum(PBI_finished_per_sprint)
+PBI_average = np.average(PBI_finished_per_sprint)
+PBI_stddeviation = np.std(PBI_finished_per_sprint)
 dates = ['15/04/20', '21/04/20', '06/05/20', '19/05/20', '02/06/20', '16/06/20', '30/06/20', '14/07/20', '28/07/20',
          '11/08/20', '25/08/20', '09/09/20', '22/09/20', '08/10/20', '20/10/20', '03/11/20', '17/11/20', '01/12/20',
          '15/12/20', '12/01/21', '26/01/21', '09/02/21', '24/02/21', '10/03/21', '23/03/21', '06/04/21', '20/04/21',
@@ -26,6 +27,21 @@ def calc_range_until_finsihed_with(number):
         number_of_sprints += 1
 
     return number_of_sprints - 1
+
+def monte_carlo_simulation(trials: int):
+    sprintdistribution = []
+    for _ in range(trials):
+        sprints = 0
+        PBI_finsished = PBI_cumulative[-1]
+        while PBI_finsished < number_PBI_to_be_finished:
+            PBI_finsished += random.choice(PBI_finished_per_sprint)
+            sprints += 1
+        sprintdistribution.append(sprints-1)
+
+    return sprintdistribution
+
+distribution = monte_carlo_simulation(10000)
+check = random.choices(PBI_finished_per_sprint, k=1000)
 
 result_average = calc_range_until_finsihed_with(PBI_average)
 result_min = calc_range_until_finsihed_with(PBI_average - PBI_stddeviation)
@@ -43,14 +59,29 @@ forecast_average = np.cumsum(future_average_array)
 forecast_min = np.cumsum(future_min_array)
 forecast_max = np.cumsum(future_max_array)
 
+perc = np.percentile(distribution, [70, 80, 85, 95])
 y = np.array([number_PBI_to_be_finished for x in range(len(dates))])
 
-plt.plot(dates[0:len(PBI_finished)], PBI_cumulative)
-plt.plot(dates[len(PBI_finished)-1:len(PBI_finished)-1 + len(forecast_average)], forecast_average)
-plt.plot(dates[len(PBI_finished)-1:len(PBI_finished)-1 + len(forecast_min)], forecast_min)
-plt.plot(dates[len(PBI_finished)-1:len(PBI_finished)-1 + len(forecast_max)], forecast_max)
+plt.figure(0)
+plt.hist(distribution)
+plt.title('MC distribution')
+
+plt.figure(1)
+plt.plot(dates[0:len(PBI_finished_per_sprint)], PBI_cumulative)
+plt.plot(dates[len(PBI_finished_per_sprint) - 1:len(PBI_finished_per_sprint) - 1 + len(forecast_average)], forecast_average)
+plt.plot(dates[len(PBI_finished_per_sprint) - 1:len(PBI_finished_per_sprint) - 1 + len(forecast_min)], forecast_min)
+plt.plot(dates[len(PBI_finished_per_sprint) - 1:len(PBI_finished_per_sprint) - 1 + len(forecast_max)], forecast_max)
 plt.plot(dates, y)
 plt.gcf().autofmt_xdate()
+
+plt.figure(2)
+plt.title('PBI finsished distribution')
+plt.hist(PBI_finished_per_sprint)
+
+plt.figure(3)
+plt.hist(check)
+plt.title('check')
+
 plt.show()
 
 
